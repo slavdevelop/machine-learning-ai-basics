37a0beddde42640991916853381824db9cb4aa71
Slavi Shtipkov <development@shtipkov.com>
2019-12-28 22:22:34 +0200

    Implemented examples of the KMeansClustering algorithm

---
+++ KMeans/KMeans_Clustering.py
@@ -0,0 +1,35 @@
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+np.random.seed(42)
+
+digits = load_digits()
+data = scale(digits.data)
+
+
+n_samples, n_features = data.shape
+n_digits = len(np.unique(digits.target))
+labels = digits.target
+
+sample_size = 300
+
+print("n_digits: %d, \t n_samples %d, \t n_features %d" % (n_digits, n_samples, n_features))
+
+print(82 * '_')
+print('init\t\ttime\tinertia\thomo\tcompl\tv-meas\tARI\tAMI\tsilhouette')
+
+
+def bench_k_means(estimator, name, data):
+    t0 = time()
+    estimator.fit(data)
+    print('%-9s\t%.2fs\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, (time() - t0), estimator.inertia_,
+             metrics.homogeneity_score(labels, estimator.labels_),
+             metrics.completeness_score(labels, estimator.labels_)))
